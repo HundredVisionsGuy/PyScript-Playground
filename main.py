af06245ebbb642cdf9ec95a0f8d9fe9b60e74099
hundredvisionsguy <winikkc@hsd.k12.or.us>
2023-04-27 15:56:00 -0700

    Adding some dev dependencies

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ to the home page."""
 import json
 from pyodide.ffi import create_proxy
 from js import document
-from js import window
 
 
 # get the project data as a dict
 @create_proxy
 def load_content(*args):
-    with open('project_data.json') as f:
+    with open("project_data.json") as f:
         data = json.load(f)
-    projects = data.get('projects')
+    projects = data.get("projects")
     menu = create_menu(projects)
-    document.getElementById('hello').innerHTML = menu
+    document.getElementById("hello").innerHTML = menu
+
 
 def create_menu(projects):
     menu = "<ul>"
@@ -25,11 +25,10 @@ def create_menu(projects):
         title = project.get("project_title")
         href = project.get("project_dir")
         href += project.get("project_filename")
-        description = project.get("project_description")
-        li = '<li><a href="' + href + '">' + title + '</a></li>'
-        menu += li 
+        li = '<li><a href="' + href + '">' + title + "</a></li>"
+        menu += li
     menu += "</ul>"
     return menu
-    
+
 
 load_content()
